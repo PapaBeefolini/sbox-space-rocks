e6d3cd9441b96030a93b111d3371bc8af3528182
ceitine <ceitinee@gmail.com>
2023-07-31 00:55:44 +0300

    fix game

--- code/ui/GameUI.cs
+++ code/ui/GameUI.cs
@@ -1,25 +1,22 @@
-﻿using Sandbox.UI;
+﻿namespace SpaceRocks.UI;
 
-namespace SpaceRocks.UI
+public partial class GameUI : Sandbox.HudEntity<RootPanel>
 {
-	public partial class GameUI : Sandbox.HudEntity<RootPanel>
+	public GameUI()
 	{
-		public GameUI()
-		{
-			if ( !IsClient )
-				return;
+		if ( !Game.IsClient )
+			return;
 
-			RootPanel.StyleSheet.Load( "/ui/Hud.scss" );
+		RootPanel.StyleSheet.Load( "/ui/Hud.scss" );
 
-			//RootPanel.AddChild<NameTags>();
-			//RootPanel.AddChild<CrosshairCanvas>();
-			ChatBox chat = RootPanel.AddChild<ChatBox>();
-			chat.Style.FontFamily = "VCR_OSD_MONO_1.001";
-			RootPanel.AddChild<VoiceList>();
-			//RootPanel.AddChild<KillFeed>();
-			//RootPanel.AddChild<Scoreboard<ScoreboardEntry>>();
-			RootPanel.AddChild<Hud>();
-			RootPanel.AddChild<GameOver>();
-		}
+		//RootPanel.AddChild<NameTags>();
+		//RootPanel.AddChild<CrosshairCanvas>();
+		ChatBox chat = RootPanel.AddChild<ChatBox>();
+		chat.Style.FontFamily = "VCR_OSD_MONO_1.001";
+		RootPanel.AddChild<VoiceList>();
+		//RootPanel.AddChild<KillFeed>();
+		//RootPanel.AddChild<Scoreboard<ScoreboardEntry>>();
+		RootPanel.AddChild<Hud>();
+		RootPanel.AddChild<GameOver>();
 	}
 }
